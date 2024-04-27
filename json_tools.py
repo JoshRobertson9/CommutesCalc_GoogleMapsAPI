c20abb72a3567a2144e73e4c64da6b99f556b15b
JoshRobertson9 <rosh.jobertson@gmail.com>
2024-04-26 21:39:58 -0400

    Enabled API Call Caching
    API calls are now cached to reduce the number of API calls.

---
+++ json_tools.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+# I tried to make my own solution, but decided to go with an existing solution
+# 
+
+"""
+def get_data_from_cache(file_name):
+    try:
+        with open('file_name', 'r') as file:
+            cache = json.load(file)
+        return cache
+    except FileNotFoundError:
+        return {}
+"""
+
+# Updates empty or existing json file
+def update_cache(file_name, new_data):
+    try:
+        with open(file_name, 'r') as file:
+            try:
+                cache = json.load(file)
+            except json.decoder.JSONDecodeError:
+                # If file is empty or not valid JSON, initialize an empty cache
+                cache = {}
+    except FileNotFoundError:
+        cache = {}
+
+    print("test")
+    print(cache)
+    print("testtt")
+    cache.update(new_data)
+    print(new_data)
+
+    with open(file_name, 'a') as file:
+        pass
+        json.dump(cache, file, indent = 4)
+
+
+
